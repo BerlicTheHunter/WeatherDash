72291c1de00bb210139a7d8f8d17dada9f6982d7
Berlic <bsorrell3@gmail.com>
2021-08-02 22:49:06 -0400

    search function and saved search btns

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,28 @@
-// ...............OpenWeather API Info...............\\
-// API Key = c217ece50a8addb1ac8b2f4eb17981a4
-const owKey = 'c217ece50a8addb1ac8b2f4eb17981a4';
+// Variables and DOM Elements
 var lat;
 var lon;
 var currentDataList= document.getElementById("currentDataList");
-var weartherULEl= document.querySelectorAll("ul");
-console.log(weartherULEl);
-// ...............Use Geolocation for Weather before First Search...............\\
-navigator.geolocation.getCurrentPosition(success);
-function success(pos){
+var weatherULEl= document.querySelectorAll("ul");
+var searchCityEl= document.getElementById("citySearch");
+var searchCity= searchCityEl.innerText;
+var searchBtn= document.getElementById('searchBtn');
+var saveBtnEl= document.getElementById('saveBtnEl');
+var savedBtn= document.getElementsByClassName("saved")
+//Use Geolocation for Weather before First Search\\
+if (searchCity==""){
+    navigator.geolocation.getCurrentPosition(success);
+    function success(pos){
     lat= pos.coords.latitude;
     lon= pos.coords.longitude;
     lat= lat.toString();
     lon= lon.toString();
-    getWeather(lat,lon);
-}
-
-// ...............Openweather OneCall APi...............\\
+    searchCity= "Current Location";
+    console.log(searchCity);
+    getWeather(lat,lon,searchCity);
+}};
+//OpenWeather API Info\\
+const owKey = 'c217ece50a8addb1ac8b2f4eb17981a4';
+// Openweather OneCall APi\\
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // {lat}= latitude
 // {lon}= longitude
@@ -30,7 +36,7 @@ function success(pos){
 // {limit}= 3
 
 function getLatLon(){
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Columbus&limit=3&appid="+owKey)
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+searchCity+"&limit=3&appid="+owKey)
     .then(
         function(response){
             if(response.status !== 200){
@@ -40,18 +46,25 @@ function getLatLon(){
         
         response.json().then(function(data){
             console.log(data);
-            lat= data[0].lat;
-            lon= data[0].lon;
-            lat= lat.toString();
-            lon= lon.toString();
-            getWeather(lat,lon);
-            });
+            if(data.length !== 0){
+                lat= data[0].lat;
+                lon= data[0].lon;
+                lat= lat.toString();
+                lon= lon.toString();
+                console.log(searchCity);
+                getWeather(lat,lon,searchCity);
+                savedSearches(searchCity);
+            }
+            else{
+                alert("City Not Found");
+            };
+        });
         }
     )    
 };
 
 function getWeather(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely,hourly,alerts&units=imperial&appid='+owKey)
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely,hourly,alerts,current&units=imperial&appid='+owKey)
     .then(function(response){
             if(response.status !== 200){
                 console.log("error with request:"+response.status);
@@ -60,29 +73,85 @@ function getWeather(){
             response.json()
             .then(function(data){
                 console.log(data);
-                // sets the current weater parameters
-                var temp=data.current.temp;
-                var windSpeed=data.current.wind_speed;
-                var humid= data.current.humidity;
-                var uvIndex= data.current.uvi;
-                var iconID= data.current.weather[0].icon;
-                // format date based on provided time
-                var utcDate= data.current.dt;
-                var date= new Date(utcDate*1000);
-                var day= date.getDate();
-                var month= date.getMonth() + 1;
-                var year= date.getFullYear();
-                var dateFormatted= month.toString()+'/'+day.toString()+'/'+year.toString();
-                               
-                // writes the current weather to the UL for current weather
-                currentDataList.innerHTML= "<li>"+dateFormatted+"</li> <li><img src='http://openweathermap.org/img/wn/"+iconID.toString()+".png'></li> <li>Temp: "+temp.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li> <li>UV Index: "+uvIndex.toString()+"</li>";
+               
+                for(i=0; i< weatherULEl.length; i++){
+                    var tempDay=data.daily[i].temp.day;
+                    var windSpeed=data.daily[i].wind_speed;
+                    var humid= data.daily[i].humidity;
+                    var uvIndex= data.daily[i].uvi;
+                    var iconID= data.daily[i].weather[0].icon;
+                    // format date based on provided time
+                    var utcDate= data.daily[i].dt;
+                    var date= new Date(utcDate*1000);
+                    var day= date.getDate();
+                    var month= date.getMonth() + 1;
+                    var year= date.getFullYear();
+                    var dateFormatted= month.toString()+'/'+day.toString()+'/'+year.toString();
+                                
+                    // writes the current weather to the UL for current weather
+                    if (i === 0){
+                        currentDataList.innerText= searchCity +"  ("+dateFormatted+")";
+
+                        weatherULEl[i].innerHTML= "<li><img src='http://openweathermap.org/img/wn/"+iconID.toString()+".png'></li><li>Temp: "+tempDay.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li> <li id='uvI'>UV Index: "+uvIndex.toString()+"</li>";
+                    }
+                    else{
+                        weatherULEl[i].innerHTML= "<li><label>Date:</label>"+dateFormatted+"</li> <li><img src='http://openweathermap.org/img/wn/"+iconID.toString()+".png'></li> <li>Temp: "+tempDay.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li>"
+                    }
+                };
+
             });
             
         }
     )
-    
-    
-
-
 }
 
+function savedSearches(){
+    if(localStorage.savedCity == undefined){
+        var savedCity=[searchCity];       
+        localStorage.savedCity= JSON.stringify(savedCity);
+    }
+    else{
+        var newSearch=[searchCity];
+        var currentList = localStorage.savedCity
+        var savedCity= JSON.parse(currentList);
+        savedCity= savedCity.concat(newSearch);
+        localStorage.savedCity= JSON.stringify(savedCity);
+    }
+    makeSavedBtns();
+};    
+  
+function makeSavedBtns(){
+    if(localStorage.savedCity != undefined){
+        var currentList = localStorage.savedCity
+        var savedCity= JSON.parse(currentList);
+        console.log(savedCity)
+        for(i=0; i<savedCity.length; i++){
+            if(i === 0){
+                var savedBtnCity="<button class='btn my-2 btn-secondary w-100 saved'>"+savedCity[0]+"</button>";
+            }
+            else{
+                savedBtnCity= savedBtnCity+"<button class='btn my-2 btn-secondary w-100 saved'>"+savedCity[i]+"</button>";
+            };
+        };
+        console.log(savedBtnCity);
+        saveBtnEl.innerHTML= savedBtnCity;
+    };
+};
+
+makeSavedBtns();
+var savedBtn= document.getElementsByClassName("saved");
+
+searchBtn.addEventListener("click",function(){
+    console.log("clicked");
+    searchCity= searchCityEl.value;
+    console.log(searchCity);
+    getLatLon(searchCity);
+});
+
+for (i of savedBtn){
+    i.addEventListener("click",function(){
+        console.log(this.innerText);
+        searchCity=this.innerText;
+        getLatLon(searchCity);
+    });
+};
