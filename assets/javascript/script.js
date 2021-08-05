8d1fda422652efbf314ed4366d5287a828e87a7e
Berlic <bsorrell3@gmail.com>
2021-08-05 12:57:26 -0400

    corrected fetch/then/then issue. improved formatting. added comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,7 +9,7 @@ var searchBtn= document.getElementById('searchBtn');
 var saveBtnEl= document.getElementById('saveBtnEl');
 var savedBtn= document.querySelectorAll("saved")
 
-//Use Geolocation for Weather before First Search\\
+//Use Geolocation to get current locations\\
 function currentLocal(){
     if (searchCity==""){
         navigator.geolocation.getCurrentPosition(success);
@@ -27,19 +27,13 @@ function currentLocal(){
 
 //OpenWeather API Info\\
 const owKey = 'c217ece50a8addb1ac8b2f4eb17981a4';
-// Openweather OneCall APi\\
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-// {lat}= latitude
-// {lon}= longitude
-// {part}= part to exclude mostly minuetly, hourly, and alerts
 
-// ...............OpenWeather Geoloction API...............\\
+// OpenWeather Geoloction API\\
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 // {city name}= city name from search input
 // {state code}= not used  
 // {country code}= not used
 // {limit}= 3
-
 function getLatLon(){
     fetch("https://api.openweathermap.org/geo/1.0/direct?q="+searchCity+"&limit=3&appid="+owKey)
     .then(
@@ -68,56 +62,59 @@ function getLatLon(){
     
 };
 
+// Openweather OneCall APi\\
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// {lat}= latitude
+// {lon}= longitude
+// {part}= part to exclude mostly minuetly, hourly, and alerts
 function getWeather(){
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely,hourly,alerts,current&units=imperial&appid='+owKey)
     .then(function(response){
-            if(response.status !== 200){
-                console.log("error with request:"+response.status);
-                return;
-            }
-            response.json()
-            .then(function(data){
-                console.log(data);
-                for(i=0; i< weatherULEl.length; i++){
-                    var tempDay=data.daily[i].temp.day;
-                    var windSpeed=data.daily[i].wind_speed;
-                    var humid= data.daily[i].humidity;
-                    var uvIndex= data.daily[i].uvi;
-                    var iconID= data.daily[i].weather[0].icon;
-                    // format date based on provided time
-                    var utcDate= data.daily[i].dt;
-                    var date= new Date(utcDate*1000);
-                    var day= date.getDate();
-                    var month= date.getMonth() + 1;
-                    var year= date.getFullYear();
-                    var dateFormatted= month.toString()+'/'+day.toString()+'/'+year.toString();
-                                
-                    // writes the current weather to the UL for current weather
-                    if (i === 0){
-                        currentDataList.innerText= searchCity +"  ("+dateFormatted+")";
-                        weatherULEl[i].innerHTML= "<li><img src='https://openweathermap.org/img/wn/"+iconID.toString()+".png'></li><li>Temp: "+tempDay.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li> <li id='uvI'>UV Index: "+uvIndex.toString()+"</li>";
-                        var uvIEl= document.getElementById('uvI');
-                        console.log(uvIEl);
-                        if(uvIndex <= 2){
-                            uvIEl.classList.add("low");
-                        }
-                        else if (uvIndex > 7){
-                            uvIEl.classList.add("high");
-                        }
-                        else{
-                            uvIEl.classList.add("medium");
-                        }
-                    }
-                    else{
-                        weatherULEl[i].innerHTML= "<li><label>Date:</label>"+dateFormatted+"</li> <li><img src='https://openweathermap.org/img/wn/"+iconID.toString()+".png'></li> <li>Temp: "+tempDay.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li>"
-                    }
-                };
-            })
-            
+        if(response.status !== 200){
+            console.log("error with request:"+response.status);
         }
-    )
-}
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        for(i=0; i< weatherULEl.length; i++){
+            var tempDay=data.daily[i].temp.day;
+            var windSpeed=data.daily[i].wind_speed;
+            var humid= data.daily[i].humidity;
+            var uvIndex= data.daily[i].uvi;
+            var iconID= data.daily[i].weather[0].icon;
+            // format date based on provided time
+            var utcDate= data.daily[i].dt;
+            var date= new Date(utcDate*1000);
+            var day= date.getDate();
+            var month= date.getMonth() + 1;
+            var year= date.getFullYear();
+            var dateFormatted= month.toString()+'/'+day.toString()+'/'+year.toString();
+                        
+            // writes the current weather to the UL for current weather
+            if (i === 0){
+                currentDataList.innerText= searchCity +"  ("+dateFormatted+")";
+                weatherULEl[i].innerHTML= "<li><img src='https://openweathermap.org/img/wn/"+iconID.toString()+".png'></li><li>Temp: "+tempDay.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li> <li id='uvI'>UV Index: "+uvIndex.toString()+"</li>";
+                var uvIEl= document.getElementById('uvI');
+                console.log(uvIEl);
+                if(uvIndex <= 2){
+                    uvIEl.classList.add("low");
+                }
+                else if (uvIndex > 7){
+                    uvIEl.classList.add("high");
+                }
+                else{
+                    uvIEl.classList.add("medium");
+                }
+            }
+            else{
+                weatherULEl[i].innerHTML= "<li><label>Date:</label>"+dateFormatted+"</li> <li><img src='https://openweathermap.org/img/wn/"+iconID.toString()+".png'></li> <li>Temp: "+tempDay.toString()+"</li> <li>Wind: "+windSpeed.toString()+" MPH</li> <li>Humidity: "+humid.toString()+"%</li>"
+            }
+        };
+    });
+};
 
+// Save Searches to local storage
 function savedSearches(){
     if(localStorage.savedCity == undefined){
         var savedCity=[searchCity];       
@@ -132,7 +129,8 @@ function savedSearches(){
     };
     makeSavedBtns();
 };    
-  
+
+// Make buttons for saved searched
 function makeSavedBtns(){
     if(localStorage.savedCity != undefined){
         var currentList = localStorage.savedCity
@@ -153,11 +151,15 @@ function makeSavedBtns(){
     };
 };
 
+// Get local weather upon opening
 currentLocal();
+
+// Get saved search buttons on opening
 makeSavedBtns();
 var savedBtn= document.getElementsByClassName("saved");
 findBtns();
 
+// Add event listener to search button.
 searchBtn.addEventListener("click",function(){
     console.log("clicked");
     searchCity= searchCityEl.value;
@@ -169,6 +171,7 @@ searchBtn.addEventListener("click",function(){
     };
 });
 
+// Apply event listeners to saved search buttons.
 function findBtns(){
     Array.from(savedBtn).forEach(function(index){
         index.addEventListener("click",function(){
